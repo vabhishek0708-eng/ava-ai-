0574f6b59d047a040b4f46bdc45f2f87d88c5490
vabhishek0708-eng <vabhishek0708@gmail.com>
2025-09-26 21:34:58 -0500

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+// src/lib/utils.ts
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function cn(...inputs: any[]) {
+  return twMerge(clsx(inputs));
 }
